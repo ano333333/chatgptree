9881b647c0c3a337535a279c12ea551cf02d45cf
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-06-17 02:31:15 +0900

    add: tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  test: {
+    projects: [
+      {
+        test: {
+          include: ["tests/**/*.test.ts"],
+          name: "unit",
+          environment: "node",
+        },
+      },
+      {
+        test: {
+          include: ["tests/**/*.browser.test.{ts,tsx}"],
+          name: "browser",
+          browser: {
+            provider: "playwright",
+            enabled: true,
+            instances: [{ browser: "chromium" }],
+          },
+        },
+      },
+    ],
+  },
+});

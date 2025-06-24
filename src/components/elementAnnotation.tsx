986f33a811722ab48b48917d0d372c936b70192d
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-06-24 14:41:28 +0900

    feat: アノテーション用のコンポーネント

---
+++ src/components/elementAnnotation.tsx
@@ -0,0 +1,114 @@
+import { useRef, useState, type FC } from "react";
+import { Info } from "lucide-react";
+
+interface ElementAnnotationProps {
+  className?: string;
+  objectRef: Element | null;
+  toggleBtnPosDiff: {
+    x: number;
+    y: number;
+  };
+  text: string;
+}
+
+/**
+ * 要素に注釈を表示するコンポーネント
+ * 長文を表示せずIDを示すのみに止め、より詳細な説明はstorybook等で別途表示する。
+ *
+ * @param props.className - コンテナのclassName
+ * @param props.objectRef - 注釈を表示する対象のDOM要素
+ * @param props.toggleBtnPosDiff - アノテーションの左上座標 - 要素左上座標（x, y座標）
+ * @param props.text - 注釈に表示するテキスト
+ * @param props.children - 注釈に表示する子要素
+ *
+ * @example
+ * ```tsx
+ * const [button, setButton] = useState<HTMLButtonElement | null>(null);
+ * return (
+ * <>
+ *   <button ref={setButton} type="button">click me!</button>
+ *   <ElementAnnotation
+ *     objectRef={button}
+ *     toggleBtnPosDiff={{ x: -70, y: 0 }}
+ *     text="example"
+ *   />
+ * </>
+ * ```
+ */
+export const ElementAnnotation: FC<ElementAnnotationProps> = (props) => {
+  const [toggleButtonPos, setToggleButtonPos] = useState({ x: 0, y: 0 });
+
+  const calcToggleButtonCenter = () => {
+    const rect = props.objectRef?.getBoundingClientRect();
+    return rect
+      ? {
+          x: rect.left + props.toggleBtnPosDiff.x,
+          y: rect.top + props.toggleBtnPosDiff.y,
+        }
+      : { x: 0, y: 0 };
+  };
+
+  // objectRefのobserverを保存する
+  const observer = useRef<IntersectionObserver | null>(null);
+  // observerは、監視対象が変更されたら更新する
+  const prevObjectRef = useRef<Element | null>(null);
+  if (prevObjectRef.current !== props.objectRef) {
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+    if (props.objectRef) {
+      observer.current = new IntersectionObserver(
+        () => {
+          setToggleButtonPos(calcToggleButtonCenter());
+        },
+        {
+          root: null,
+          threshold: 0,
+        },
+      );
+      observer.current.observe(props.objectRef);
+    }
+    setToggleButtonPos(calcToggleButtonCenter());
+    prevObjectRef.current = props.objectRef;
+  }
+
+  const calcToggleButtonContainerClassname = () => {
+    const classNames = [
+      "fixed",
+      "z-10",
+      "bg-white",
+      "rounded-md",
+      "p-2",
+      "flex",
+      "items-center",
+      "gap-2",
+      "border-gray-300",
+      "border",
+      "shadow-md",
+    ];
+    if (!props.objectRef) {
+      classNames.push("hidden");
+    }
+    return classNames.join(" ");
+  };
+
+  const calcToggleButtonClassname = () => {
+    const classNames = ["stroke-blue-400", "inline"];
+    return classNames.join(" ");
+  };
+
+  return (
+    <>
+      <div
+        className={`${calcToggleButtonContainerClassname()} ${props.className}`}
+        style={{
+          left: `${Math.floor(toggleButtonPos.x)}px`,
+          top: `${Math.floor(toggleButtonPos.y)}px`,
+        }}
+      >
+        <Info className={calcToggleButtonClassname()} />
+        <p className="text-sm">{props.text}</p>
+      </div>
+    </>
+  );
+};
---
+++ src/stories/elementAnnotation.stories.tsx
@@ -0,0 +1,104 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { ElementAnnotation } from "../components/elementAnnotation";
+import { useRef, useState } from "react";
+
+const meta: Meta<typeof ElementAnnotation> = {
+  title: "Components/ElementAnnotation",
+  component: ElementAnnotation,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// 基本的な表示ストーリー
+export const Display: Story = {
+  render: () => {
+    const [button, setButton] = useState<HTMLButtonElement | null>(null);
+    const buttonCallbackRef = (e: HTMLButtonElement | null) => {
+      setButton(e);
+    };
+    return (
+      <>
+        <button ref={buttonCallbackRef} type="button">
+          click me!
+        </button>
+        <ElementAnnotation
+          objectRef={button}
+          toggleBtnPosDiff={{ x: -70, y: 0 }}
+          text="1"
+        />
+      </>
+    );
+  },
+};
+
+// ボタンクリックで非表示になるストーリー
+export const DisplayWithHide: Story = {
+  render: () => {
+    const [button, setButton] = useState<HTMLButtonElement | null>(null);
+    const buttonCallbackRef = (e: HTMLButtonElement | null) => {
+      setButton(e);
+    };
+    return (
+      <>
+        <button
+          ref={buttonCallbackRef}
+          type="button"
+          onClick={() => {
+            if (button) {
+              button.style.display = "none";
+            }
+          }}
+        >
+          click me!
+        </button>
+        <ElementAnnotation
+          objectRef={button}
+          toggleBtnPosDiff={{ x: -70, y: 0 }}
+          text="1"
+        />
+      </>
+    );
+  },
+};
+
+export const DisplayWithSwap: Story = {
+  render: () => {
+    const [button, setButton] = useState<HTMLButtonElement | null>(null);
+    const button1Ref = useRef<HTMLButtonElement | null>(null);
+    const button2Ref = useRef<HTMLButtonElement | null>(null);
+    const onClick = (b: HTMLButtonElement | null) => {
+      setButton(b);
+    };
+    return (
+      <>
+        <div className="flex gap-2">
+          <button
+            ref={button1Ref}
+            type="button"
+            onClick={() => onClick(button1Ref.current)}
+          >
+            click me!
+          </button>
+          <button
+            ref={button2Ref}
+            type="button"
+            onClick={() => onClick(button2Ref.current)}
+          >
+            click me!
+          </button>
+        </div>
+        <ElementAnnotation
+          objectRef={button}
+          toggleBtnPosDiff={{ x: 0, y: -50 }}
+          text="1"
+        />
+      </>
+    );
+  },
+};

1558733cedb66df91c6afc425920599f065587e2
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-06-25 02:04:41 +0900

    add: init shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     projects: [
       {

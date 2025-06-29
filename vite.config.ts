9eef1cb8b20290d418378aa81969d60ded823231
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-06-29 23:35:23 +0900

    ci: production下のみベースパスを設定

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/chatgptree/" : "/",
   test: {
     projects: [
       {

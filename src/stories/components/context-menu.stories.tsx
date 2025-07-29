e7ccdcfbffa0ca253bd2b84ffff0f2ff555b3e5c
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-07-30 01:12:52 +0900

    docs: 新しいContextMenu仕様にContextMenu Storybookを追従

--- src/stories/components/context-menu.stories.tsx
+++ src/stories/components/context-menu.stories.tsx
@@ -1,12 +1,15 @@
 import {
   ContextMenu,
+  ContextMenuContext,
   ContextMenuItem,
   ContextMenuSeparator,
   ContextMenuSubMenu,
   ContextMenuSubMenuRoot,
   ContextMenuSubMenuTrigger,
+  type ContextMenuElement,
 } from "@/components/context-menu";
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { useRef } from "react";
 import { action } from "storybook/actions";
 
 const meta: Meta<typeof ContextMenu> = {
@@ -22,39 +25,75 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const ContextMenuStory: Story = {
-  render: () => (
-    <ContextMenu screenPosition={{ x: 0, y: 0 }}>
-      <ContextMenuItem key="test1" onClick={action("test1")}>
-        test1
-      </ContextMenuItem>
-      <ContextMenuItem key="test2" onClick={action("test2")}>
-        test2
-      </ContextMenuItem>
-      <ContextMenuSeparator />
-      <ContextMenuSubMenuRoot>
-        <ContextMenuSubMenuTrigger>submenu</ContextMenuSubMenuTrigger>
-        <ContextMenuSubMenu>
-          <ContextMenuItem>submenu item 1</ContextMenuItem>
-          <ContextMenuItem>submenu item 2</ContextMenuItem>
-        </ContextMenuSubMenu>
-      </ContextMenuSubMenuRoot>
-      <ContextMenuSeparator />
-      <ContextMenuSubMenuRoot>
-        <ContextMenuSubMenuTrigger>2level-submenu</ContextMenuSubMenuTrigger>
-        <ContextMenuSubMenu>
-          <ContextMenuItem>2level-submenu item 1</ContextMenuItem>
-          <ContextMenuSubMenuRoot>
-            <ContextMenuSubMenuTrigger>
-              2level-submenu trigger
-            </ContextMenuSubMenuTrigger>
-            <ContextMenuSubMenu>
-              <ContextMenuItem>2level-submenu item 2</ContextMenuItem>
-              <ContextMenuSeparator />
-              <ContextMenuItem>2level-submenu item 3</ContextMenuItem>
-            </ContextMenuSubMenu>
-          </ContextMenuSubMenuRoot>
-        </ContextMenuSubMenu>
-      </ContextMenuSubMenuRoot>
-    </ContextMenu>
-  ),
+  render: () => {
+    const contextMenuRef = useRef<ContextMenuElement>(null);
+    return (
+      <>
+        <div>
+          <button
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              contextMenuRef.current?.setContextMenuState({
+                status: "open",
+                position: { x: 0, y: 0 },
+              });
+            }}
+          >
+            open
+          </button>
+        </div>
+        <div
+          className="absolute top-[200px] left-[200px] w-[200px] h-[200px] bg-red-500"
+          onContextMenu={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            contextMenuRef.current?.setContextMenuState({
+              status: "open",
+              position: { x: e.clientX, y: e.clientY },
+            });
+          }}
+        >
+          右クリックでコンテキストメニューを開く
+        </div>
+        <ContextMenuContext>
+          <ContextMenu initialPosition={{ x: 0, y: 0 }} ref={contextMenuRef}>
+            <ContextMenuItem key="test1" onClick={action("test1")}>
+              test1
+            </ContextMenuItem>
+            <ContextMenuItem key="test2" onClick={action("test2")}>
+              test2
+            </ContextMenuItem>
+            <ContextMenuSeparator />
+            <ContextMenuSubMenuRoot>
+              <ContextMenuSubMenuTrigger>submenu</ContextMenuSubMenuTrigger>
+              <ContextMenuSubMenu>
+                <ContextMenuItem>submenu item 1</ContextMenuItem>
+                <ContextMenuItem>submenu item 2</ContextMenuItem>
+              </ContextMenuSubMenu>
+            </ContextMenuSubMenuRoot>
+            <ContextMenuSeparator />
+            <ContextMenuSubMenuRoot>
+              <ContextMenuSubMenuTrigger>
+                2level-submenu
+              </ContextMenuSubMenuTrigger>
+              <ContextMenuSubMenu>
+                <ContextMenuItem>2level-submenu item 1</ContextMenuItem>
+                <ContextMenuSubMenuRoot>
+                  <ContextMenuSubMenuTrigger>
+                    2level-submenu trigger
+                  </ContextMenuSubMenuTrigger>
+                  <ContextMenuSubMenu>
+                    <ContextMenuItem>2level-submenu item 2</ContextMenuItem>
+                    <ContextMenuSeparator />
+                    <ContextMenuItem>2level-submenu item 3</ContextMenuItem>
+                  </ContextMenuSubMenu>
+                </ContextMenuSubMenuRoot>
+              </ContextMenuSubMenu>
+            </ContextMenuSubMenuRoot>
+          </ContextMenu>
+        </ContextMenuContext>
+      </>
+    );
+  },
 };

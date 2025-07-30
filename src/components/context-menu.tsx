9fc798b0325d9d755c6e9ff1c595777f7fcb1812
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-07-31 02:27:39 +0900

    feat: ContextMenuContext実装とそれに合わせたContextMenuの修正

--- src/components/context-menu.tsx
+++ src/components/context-menu.tsx
@@ -1,26 +1,122 @@
 import { ChevronRight } from "lucide-react";
 import {
   createContext,
+  useCallback,
   useContext,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
   useState,
   type KeyboardEventHandler,
   type MouseEventHandler,
   type ReactNode,
   type RefObject,
+  type MouseEvent,
+  type KeyboardEvent,
+  useId,
 } from "react";
 
 interface ContextMenuContextProps {
   children: ReactNode;
 }
 
+const contextMenuContext = createContext<{
+  /**
+   * ContextMenuが自身を開くことを通達する。
+   * ContextMenuContextは、既に開いているコンテキストメニューを閉じる。
+   * @param id 新しいContextMenuのID
+   * @param close 新しいContextMenuを閉じるための関数
+   */
+  openCurrentContextMenu: (id: string, close: () => void) => void;
+  /**
+   * 既に開いているContextMenuを閉じる。
+   */
+  closeCurrentContextMenu: () => void;
+}>({
+  openCurrentContextMenu: () => {
+    throw new Error("ContextMenuContext is not initialized");
+  },
+  closeCurrentContextMenu: () => {
+    throw new Error("ContextMenuContext is not initialized");
+  },
+});
+
 export function ContextMenuContext(props: ContextMenuContextProps) {
-  return <>{props.children}</>;
+  const currentContextMenu = useRef<{
+    id: string;
+    close: () => void;
+  } | null>(null);
+
+  const openCurrentContextMenu = useCallback(
+    (id: string, close: () => void) => {
+      const current = currentContextMenu.current;
+      if (current && current.id !== id) {
+        current.close();
+      }
+      currentContextMenu.current = { id, close };
+    },
+    [],
+  );
+  const closeCurrentContextMenu = useCallback(() => {
+    if (currentContextMenu.current) {
+      currentContextMenu.current.close();
+      currentContextMenu.current = null;
+    }
+  }, []);
+
+  /**
+   * documentのイベントを監視して、コンテキストメニュー外で
+   * - click
+   * - contextmenu
+   * - keydown
+   * のイベントが生じれば、コンテキストメニューを閉じる
+   */
+  useEffect(() => {
+    const handleDocumentClick = () => {
+      closeCurrentContextMenu();
+    };
+
+    const handleDocumentContextMenu = () => {
+      closeCurrentContextMenu();
+    };
+
+    const handleDocumentKeyDown = (e: globalThis.KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeCurrentContextMenu();
+      }
+    };
+
+    document.addEventListener("click", handleDocumentClick);
+    document.addEventListener("contextmenu", handleDocumentContextMenu);
+    document.addEventListener("keydown", handleDocumentKeyDown);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+      document.removeEventListener("contextmenu", handleDocumentContextMenu);
+      document.removeEventListener("keydown", handleDocumentKeyDown);
+    };
+  }, [closeCurrentContextMenu]);
+
+  return (
+    <contextMenuContext.Provider
+      value={{
+        openCurrentContextMenu,
+        closeCurrentContextMenu,
+      }}
+    >
+      {props.children}
+    </contextMenuContext.Provider>
+  );
 }
 
 type ContextMenuState =
   | {
       status: "open";
-      position: { x: number; y: number };
+      position: {
+        x: number;
+        y: number;
+      };
     }
   | {
       status: "close";
@@ -47,20 +143,93 @@ interface ContextMenuProps {
  * @param props.screenPosition コンテキストメニューのスクリーン座標位置
  * @param props.children (ContextMenuItem|ContextMenuSeparator|ContextMenuSubMenuRoot)[]
  */
-export function ContextMenu(props: ContextMenuProps) {
+export function ContextMenu({
+  ref,
+  initialPosition,
+  children,
+  className,
+}: ContextMenuProps) {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState<{ x: number; y: number } | null>(
+    null,
+  );
+
+  // contextMenuContextから識別するためのID
+  const contextMenuId = useId();
+
+  const { openCurrentContextMenu, closeCurrentContextMenu } = useRef(
+    useContext(contextMenuContext),
+  ).current;
+
+  useImperativeHandle(ref, () => ({
+    style: divRef.current?.style,
+    className: divRef.current?.className,
+    setContextMenuState: (state: ContextMenuState) => {
+      if (state.status === "open") {
+        setPosition(state.position);
+        openCurrentContextMenu(contextMenuId, () => {
+          setPosition(null);
+        });
+      } else {
+        setPosition(null);
+        closeCurrentContextMenu();
+      }
+    },
+  }));
+
+  const initialPositionRef = useRef(initialPosition);
+  useEffect(() => {
+    if (initialPositionRef.current) {
+      setPosition(initialPositionRef.current);
+      openCurrentContextMenu(contextMenuId, () => {
+        setPosition(null);
+      });
+    }
+    return () => {
+      setPosition(null);
+      closeCurrentContextMenu();
+    };
+  }, [openCurrentContextMenu, closeCurrentContextMenu, contextMenuId]);
+
+  const style = useMemo(() => {
+    return {
+      top: position?.y,
+      left: position?.x,
+      zIndex: 1 << 20,
+    };
+  }, [position]);
+
+  // コンテキストメニューの子要素から漏れたイベントを停止(documentまで届くとContextMenuContextにより自分が閉じられる)
+  const handleContextMenuClick = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  const handleContextMenuContextMenu = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const handleContextMenuKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div
-      className={`fixed bg-white border border-gray-200 rounded-lg shadow-lg py-1 min-w-[160px] z-[2000] ${props.className ?? ""}`}
-      style={{
-        top: props.screenPosition.y,
-        left: props.screenPosition.x,
-      }}
-      ref={props.ref}
-      data-component-name="context-menu"
-      role="menu"
-    >
-      {props.children}
-    </div>
+    <>
+      {position && (
+        <div
+          className={`fixed bg-white border border-gray-200 rounded-lg shadow-lg py-1 min-w-[160px] z-[2000] ${className ?? ""}`}
+          style={style}
+          ref={divRef}
+          data-component-name="context-menu"
+          role="menu"
+          onClick={handleContextMenuClick}
+          onContextMenu={handleContextMenuContextMenu}
+          onKeyDown={handleContextMenuKeyDown}
+        >
+          {children}
+        </div>
+      )}
+    </>
   );
 }
 

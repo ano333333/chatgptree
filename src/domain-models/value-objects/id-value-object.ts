671d3d2ed07b6775a734e997f02c266f76ceadb4
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-08-18 02:35:05 +0900

    refactor: シリアライズのためのコンストラクタ引数を追加

--- src/domain-models/value-objects/id-value-object.ts
+++ src/domain-models/value-objects/id-value-object.ts
@@ -1,9 +1,19 @@
-import { v7 as uuidv7 } from "uuid";
+import {
+  v7 as uuidv7,
+  validate as uuidValidate,
+  version as uuidVersion,
+} from "uuid";
 
 export abstract class IDValueObject {
   private readonly _value: string;
-  constructor() {
-    this._value = uuidv7();
+  constructor(value?: string) {
+    if (
+      value !== undefined &&
+      (!uuidValidate(value) || uuidVersion(value) !== 7)
+    ) {
+      throw new Error(`Invalid ID: ${value}`);
+    }
+    this._value = value ?? uuidv7();
   }
   public equals(other: IDValueObject): boolean {
     return this._brand === other._brand && this._value === other._value;

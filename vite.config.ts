022ac2dae1fa20d1e256c0f797be045856650890
ano333333 <172483903+ano333333@users.noreply.github.com>
2025-08-24 16:02:09 +0900

    test: fake-indexeddbのvitestセットアップ

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
           include: ["tests/**/*.test.ts"],
           name: "unit",
           environment: "node",
+          setupFiles: ["fake-indexeddb/auto"],
         },
       },
       {

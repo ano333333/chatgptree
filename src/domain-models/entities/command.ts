67332ae8ba8ab31133769ff6f5b954cf3e7dcadd
ano333333 <ano333333github@gmail.com>
2025-10-06 15:12:03 +0900

    Revert "fix(needs revert): 未使用変数/インポートの削除" #43
    This reverts commit 2c4b404a9b998166d9e41f98a6572d5379468bf3.

--- src/domain-models/entities/command.ts
+++ src/domain-models/entities/command.ts
@@ -1,7 +1,9 @@
+import type { CommandUnit } from "../value-objects/command-unit";
 import { CommandId } from "../value-objects/id-value-objects/command-id";
 
 export class Command {
   public readonly id: CommandId;
+  private _units: CommandUnit[] = [];
   public readonly timestamp: Date;
   constructor(timestamp: Date) {
     this.id = new CommandId();

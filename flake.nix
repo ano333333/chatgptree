a3521c8b82ad53ec8708dafcdc16d77bee550675
ano333333 <ano333333github@gmail.com>
2025-10-06 15:07:21 +0900

    Revert "refactor: vscode+extension(biome/prettier)追加" #43 - vscodeにグローバルの拡張機能が読み込めなくなった - 結局ターミナルから「code .」する必要がありあまり意味がない
    This reverts commit 787b1a12b254c46a8fc741ee09653ea7ccaede93.

--- flake.nix
+++ flake.nix
@@ -9,24 +9,13 @@
   }:
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    builtinExts = with pkgs.vscode-extensions; [
-      biomejs.biome
-      esbenp.prettier-vscode
-    ];
-    vscode = pkgs.vscode-with-extensions.override {
-      vscodeExtensions = builtinExts;
-    };
+    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         nodejs_24
         nodePackages.pnpm
-        vscode
       ];
       shellHook = ''
         echo "entered devShell"

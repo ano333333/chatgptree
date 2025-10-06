e7b414b8d5f64f941e3cc711cc0f9398bd6e46a0
ano333333 <ano333333github@gmail.com>
2025-10-06 15:07:21 +0900

    refactor: vscode+extension(biome/prettier)追加 #43

--- flake.nix
+++ flake.nix
@@ -9,13 +9,24 @@
   }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    builtinExts = with pkgs.vscode-extensions; [
+      biomejs.biome
+      esbenp.prettier-vscode
+    ];
+    vscode = pkgs.vscode-with-extensions.override {
+      vscodeExtensions = builtinExts;
+    };
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         nodejs_24
         nodePackages.pnpm
+        vscode
       ];
       shellHook = ''
         echo "entered devShell"
